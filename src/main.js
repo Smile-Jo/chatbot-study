eb9222963e0a88c91e5026af7a308f48a930b8d5
Smile-Jo <rok519674272@gmail.com>
2025-06-23 11:38:58 +0900

    sweetalert2 추가

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
+import Swal from 'sweetalert2';
+
 const button = document.getElementById("dangerButton");
 
 button.addEventListener("click", () => {
-  alert("클릭하지 마세요");
+  Swal.fire({
+    icon: 'warning',
+    title: '정말 클릭하셨나요?',
+    text: '이 버튼은 누르지 말라고 했잖아요!',
+    confirmButtonText: '죄송합니다',
+    confirmButtonColor: '#81c784',
+    background: '#f1f8e9',
+    color: '#1b5e20',
+    backdrop: `rgba(130, 224, 170, 0.4)`
+  });
 });
